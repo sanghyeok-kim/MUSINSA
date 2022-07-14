56cff835c8a9f13dc8d25315b5e61679dc548106
sanghyeok-kim <slsnsi1212@gmail.com>
2022-07-14 16:08:16 +0900

    init: 프로젝트 생성 및 초기 설정

---
+++ MusinsaAssignmentApp/MusinsaAssignmentApp/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  MusinsaAssignmentApp
+//
+//  Created by 김상혁 on 2022/07/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
