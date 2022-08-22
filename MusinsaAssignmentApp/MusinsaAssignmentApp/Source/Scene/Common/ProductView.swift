f922a9552dfe3754eab976773c5239903f145c4c
sanghyeok-kim <slsnsi1212@gmail.com>
2022-08-22 21:12:47 +0900

    feat: GridProduct, ScrollProduct의 공용 ProductView 구현

---
+++ MusinsaAssignmentApp/MusinsaAssignmentApp/Source/Scene/Common/ProductView.swift
@@ -0,0 +1,100 @@
+//
+//  ProductView.swift
+//  MusinsaAssignmentApp
+//
+//  Created by 김상혁 on 2022/08/22.
+//
+
+import UIKit
+
+final class ProductView: UIView {
+    private lazy var thumbnailImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleToFill
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    
+    private lazy var brandNameLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 12)
+        return label
+    }()
+    
+    private lazy var priceLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 12, weight: .bold)
+        return label
+    }()
+    
+    private lazy var saleRateLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 12, weight: .bold)
+        label.textColor = .red
+        return label
+    }()
+    
+    private lazy var couponLabel: UILabel = {
+        let label = UILabel()
+        label.text = "쿠폰"
+        label.font = UIFont.systemFont(ofSize: 12)
+        label.textColor = .white
+        label.backgroundColor = .blue
+        return label
+    }()
+    
+    private lazy var productSaleStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+//        stackView.spacing = 1
+        stackView.distribution = .equalSpacing
+        [priceLabel, saleRateLabel, couponLabel].forEach {
+            stackView.addArrangedSubview($0)
+        }
+        return stackView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        layout()
+        backgroundColor = .systemGray
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure(with productDTO: ProductDTO) {
+        guard let thumbNailUrl = productDTO.thumbnailURL else { return }
+        self.thumbnailImageView.setImageWithCaching(from: thumbNailUrl)
+        
+        //guard let linkUrl = product.linkURL else { return }
+        self.brandNameLabel.text = productDTO.brandName
+        self.priceLabel.text = productDTO.price
+        self.saleRateLabel.text = productDTO.saleRate
+        self.couponLabel.isHidden = productDTO.hasCoupon
+    }
+    
+    private func layout() {
+        addSubview(thumbnailImageView)
+        addSubview(brandNameLabel)
+        addSubview(productSaleStackView)
+        
+        thumbnailImageView.translatesAutoresizingMaskIntoConstraints = false
+        thumbnailImageView.topAnchor.constraint(equalTo: topAnchor).isActive = true
+//        thumbnailImageView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+        thumbnailImageView.heightAnchor.constraint(equalTo: widthAnchor, multiplier: 1.15).isActive = true
+        thumbnailImageView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+        thumbnailImageView.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+        
+        brandNameLabel.translatesAutoresizingMaskIntoConstraints = false
+        brandNameLabel.bottomAnchor.constraint(equalTo: productSaleStackView.topAnchor, constant: -8).isActive = true
+        brandNameLabel.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+        
+        productSaleStackView.translatesAutoresizingMaskIntoConstraints = false
+        productSaleStackView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+        productSaleStackView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+        productSaleStackView.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+    }
+}
