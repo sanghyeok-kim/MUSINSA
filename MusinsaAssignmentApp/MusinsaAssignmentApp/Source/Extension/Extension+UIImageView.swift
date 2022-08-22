53fe611c166e2c3ef0737c44c86ee0d56d0ecf83
sanghyeok-kim <slsnsi1212@gmail.com>
2022-08-22 09:14:42 +0900

    feat: UIImageView extension 분리

---
+++ MusinsaAssignmentApp/MusinsaAssignmentApp/Source/Extension/Extension+UIImageView.swift
@@ -0,0 +1,21 @@
+//
+//  Extension+UIImageView.swift
+//  MusinsaAssignmentApp
+//
+//  Created by 김상혁 on 2022/08/22.
+//
+
+import Foundation
+
+extension UIImageView {
+    func setImageWithCaching(from url: URL) {
+        ImageCacheManager.shared.getImage(from: url) { result in
+            switch result {
+            case .success(let image):
+                DispatchQueue.main.async { self.image = image }
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+}
