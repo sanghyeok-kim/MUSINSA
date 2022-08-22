8e2a7b6f5f16bf4abe9f1776a68f8f3c86cf34c1
sanghyeok-kim <slsnsi1212@gmail.com>
2022-08-22 09:14:26 +0900

    simplify: ImageCacheManager 불필요한 주석 제거

--- MusinsaAssignmentApp/MusinsaAssignmentApp/Source/Network/ImageCacheManager.swift
+++ MusinsaAssignmentApp/MusinsaAssignmentApp/Source/Network/ImageCacheManager.swift
@@ -31,12 +31,12 @@ final class ImageCacheManager {
     
     private func findInDisk(from url: URL) -> UIImage? {
         let imageName = url.lastPathComponent
-        guard let diskCacheDirectoryUrl = self.diskCacheDirectoryUrl else { return nil } //유저 도메인의 캐시 디렉토리 위치
-        let filePath = diskCacheDirectoryUrl.appendingPathComponent(imageName).path //cacheDirectory 경로에 있는 이미지 파일
+        guard let diskCacheDirectoryUrl = self.diskCacheDirectoryUrl else { return nil }
+        let filePath = diskCacheDirectoryUrl.appendingPathComponent(imageName).path
         
-        if fileManager.fileExists(atPath: filePath) { //disk에 있다면
-            guard let image = UIImage(contentsOfFile: filePath) else { return nil } //disk에서 꺼내서
-            memoryCache.setObject(image, forKey: imageName as NSString) //memoryCache에 저장
+        if fileManager.fileExists(atPath: filePath) {
+            guard let image = UIImage(contentsOfFile: filePath) else { return nil }
+            memoryCache.setObject(image, forKey: imageName as NSString)
             return image
         } else {
             return nil
@@ -57,10 +57,9 @@ final class ImageCacheManager {
                 return
             }
             
-            //disk caching
             let imageName = url.lastPathComponent
-            //tempDirectoryUrl.lastPathComponent로 하면 안됨!!! 임시 폴더의 임시 파일이라 매번 이름이 달라져서 캐싱 안됨!!! ㅠㅠ
             
+            //disk caching
             guard let diskCacheDirectoryUrl = self.diskCacheDirectoryUrl else { return }
             let destinationUrl = diskCacheDirectoryUrl.appendingPathComponent(imageName)
             try? self.fileManager.copyItem(at: tempDirectoryUrl, to: destinationUrl)
@@ -83,58 +82,3 @@ final class ImageCacheManager {
         }
     }
 }
-
-extension UIImageView {
-    func setImageWithCaching(from url: URL) {
-        ImageCacheManager.shared.getImage(from: url) { result in
-            switch result {
-            case .success(let image):
-                DispatchQueue.main.async { self.image = image }
-            case .failure(let error):
-                print(error.localizedDescription)
-            }
-        }
-    }
-}
-
-
-//final class ImageManager {
-//
-//    static let shared = ImageManager()
-//
-//    private let imageCache = NSCache<NSString, UIImage>()
-//
-//    func loadImage(url: URL) async -> UIImage? {
-//        let imageName = url.lastPathComponent
-//
-//        if let cacheImage = self.imageCache.object(forKey: imageName as NSString) {
-//            return cacheImage
-//        }
-//
-//        guard let cachesDirectory = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first else {
-//            return UIImage()
-//        }
-//
-//        let destination = cachesDirectory.appendingPathComponent(imageName)
-//
-//        if FileManager.default.fileExists(atPath: destination.path),
-//           let image = UIImage(contentsOfFile: destination.path) {
-//            self.imageCache.setObject(image, forKey: imageName as NSString)
-//            return image
-//        }
-//
-//        let result = Task { () -> UIImage? in
-//            guard let (url, _) = try? await URLSession.shared.download(from: url) else {
-//                return nil
-//            }
-//            try? FileManager.default.copyItem(at: url, to: destination)
-//            guard let image = UIImage(contentsOfFile: destination.path) else {
-//                return UIImage()
-//            }
-//            self.imageCache.setObject(image, forKey: imageName as NSString)
-//            return image
-//        }
-//
-//        return await result.value
-//    }
-//}
