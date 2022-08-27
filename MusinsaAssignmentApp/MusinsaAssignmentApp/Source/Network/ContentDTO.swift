45b783cbaf87b0a8205261bec691944fa4b8cebe
sanghyeok-kim <slsnsi1212@gmail.com>
2022-08-27 23:06:51 +0900

    feat: ContentDTO 프로토콜 구현

---
+++ MusinsaAssignmentApp/MusinsaAssignmentApp/Source/Network/ContentDTO.swift
@@ -0,0 +1,13 @@
+//
+//  ContentDTO.swift
+//  MusinsaAssignmentApp
+//
+//  Created by 김상혁 on 2022/08/27.
+//
+
+import Foundation
+
+protocol ContentDTO {
+    var linkUrl: URL? { get }
+    var thumbnailUrl: URL? { get }
+}
