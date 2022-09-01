a45eda39ffe2be6442763eb00f8d58ac08f5995c
sanghyeok-kim <slsnsi1212@gmail.com>
2022-09-01 15:42:00 +0900

    refactor: MainCollectionViewDataSource가 하위 dataSource 생성시 viewModel도 함께 넣어주는 방식으로 변경

--- MusinsaAssignmentApp/MusinsaAssignmentApp/Source/Scene/Main/MainCollectionViewDataSource.swift
+++ MusinsaAssignmentApp/MusinsaAssignmentApp/Source/Scene/Main/MainCollectionViewDataSource.swift
@@ -7,45 +7,70 @@
 
 import UIKit
 
-final class MainCollectionViewDataSource: NSObject, UICollectionViewDataSource {
-    private var sectionViewModels: [SectionViewModel] = []
+final class MainCollectionViewDataSource: NSObject {
     
-    func appendSection(viewModels: [SectionViewModel]) {
-        sectionViewModels.append(contentsOf: viewModels)
-    }
+    private var sectionDataSources: [SectionDataSource] = []
     
-    //Section안의 item 개수
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        let sectionItmeCount = sectionViewModels[section].getItemCount()
-        return sectionItmeCount
-    }
-    
-    //Section의 개수
-    func numberOfSections(in collectionView: UICollectionView) -> Int {
-        let sectionCount = sectionViewModels.count
-        return sectionCount
-    }
-    
-    //각 Section별 DataSource
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let sectionDataSource = sectionViewModels[indexPath.section].dequeueReusableCell(collectionView, cellForItemAt: indexPath)
-        return sectionDataSource
+    func append(_ sectionDataSourceViewModels: [SectionDataSourceViewModel]) {
+        sectionDataSourceViewModels.forEach { sectionDataSourceViewModel in
+            switch sectionDataSourceViewModel.sectionData.contents.type { //TODO: dot level 낮추기
+            case .banner:
+                let sectionDataSource = BannerSectionDataSource()
+                sectionDataSource.viewModel = sectionDataSourceViewModel as? BannerSectionDataSourceViewModel
+                sectionDataSources.append(sectionDataSource)
+            case .grid:
+                let sectionDataSource = GridProductSectionDataSource()
+                sectionDataSource.viewModel = sectionDataSourceViewModel as? GridProductSectionDataSourceViewModel
+                sectionDataSources.append(sectionDataSource)
+            case .scroll:
+                let sectionDataSource = ScrollProductSectionDataSource()
+                sectionDataSource.viewModel = sectionDataSourceViewModel as? ScrollProductSectionDataSourceViewModel
+                sectionDataSources.append(sectionDataSource)
+            case .style:
+                let sectionDataSource = StyleSectionDataSource()
+                sectionDataSource.viewModel = sectionDataSourceViewModel as? StyleSectionDataSourceViewModel
+                sectionDataSources.append(sectionDataSource)
+            }
+        }
     }
     
     //각 Section별 CompositionalLayout
     func sectionProvider(_ section: Int, env: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection? {
-        let sectionLayout = sectionViewModels[section].sectionProvider(section, env: env)
+        let sectionLayout = sectionDataSources[section].layoutSection
         return sectionLayout
+    }
+}
+
+extension MainCollectionViewDataSource: UICollectionViewDataSource {
+    
+    //Section의 개수
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        let sectionCount = sectionDataSources.count
+        return sectionCount
+    }
+    
+    //Section안의 item 개수
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        let sectionItemCount = sectionDataSources[section].itemCount
+        return sectionItemCount
+    }
+    
+    //DataSource의 각 Section별 Item -> 여기서는 Item이 SectionDataSource를 의미
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let sectionDataSource = sectionDataSources[indexPath.section].dequeueReusableCell(collectionView, cellForItemAt: indexPath)
+        return sectionDataSource
     }
     
     //각 Section별 SupplementaryView
     func collectionView(_ collectionView: UICollectionView,
                         viewForSupplementaryElementOfKind kind: String,
                         at indexPath: IndexPath) -> UICollectionReusableView {
-        
-        let supplementaryView = sectionViewModels[indexPath.section].collectionView(collectionView,
-                                                                                viewForSupplementaryElementOfKind: kind,
-                                                                                at: indexPath)
+
+        let supplementaryView = sectionDataSources[indexPath.section].dequeueSupplementaryView(
+            collectionView,
+            viewForSupplementaryElementOfKind: kind,
+            at: indexPath
+        )
         return supplementaryView
     }
 }
