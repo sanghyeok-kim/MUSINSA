342bbdd0aa7fc856803420622b3d97a4a44d18c3
sanghyeok-kim <slsnsi1212@gmail.com>
2022-09-05 23:23:37 +0900

    feat: HeaderViewModel 구현

---
+++ MusinsaAssignmentApp/MusinsaAssignmentApp/Source/Present/Scene/SupplementaryView/HeaderViewModel.swift
@@ -0,0 +1,33 @@
+//
+//  HeaderViewModel.swift
+//  MusinsaAssignmentApp
+//
+//  Created by 김상혁 on 2022/08/22.
+//
+
+import Foundation
+
+final class HeaderViewModel: ViewModel {
+    
+    struct Action {
+        let loadData = PublishRelay<Void>()
+        let seeAllButtonTapped = PublishRelay<Void>()
+    }
+    
+    struct State {
+        let loadedData = PublishRelay<HeaderEntity>()
+    }
+    
+    let action = Action()
+    let state = State()
+    
+    private let disposeBag = DisposeBag()
+    
+    init?(header: HeaderDTO?) {
+        guard let header = header else { return }
+        action.loadData.bind { [weak self] in
+            self?.state.loadedData.accept(HeaderEntity(header: header))
+        }
+        .disposed(by: disposeBag)
+    }
+}
