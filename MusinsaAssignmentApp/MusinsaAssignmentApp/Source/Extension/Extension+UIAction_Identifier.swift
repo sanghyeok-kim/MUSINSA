7253c300b0faba19a769fa26b068a2eb9caf25d7
sanghyeok-kim <slsnsi1212@gmail.com>
2022-09-07 21:23:07 +0900

    feat: UIAction.Identifier extension으로 cellTapped Identifier 구현

---
+++ MusinsaAssignmentApp/MusinsaAssignmentApp/Source/Extension/Extension+UIAction_Identifier.swift
@@ -0,0 +1,14 @@
+//
+//  Extension+UIAction_Identifier.swift
+//  MusinsaAssignmentApp
+//
+//  Created by 김상혁 on 2022/09/07.
+//
+
+import UIKit
+
+extension UIAction.Identifier {
+    static var cellTapped: UIAction.Identifier {
+        return UIAction.Identifier("\(String(describing: self))")
+    }
+}
